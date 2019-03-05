d9cb4b34fb2773f8a5da5631371f491422d7673b
Alexander Deider <arth.inbox@gmail.com>
2019-03-05 23:32:40 +0700

    [git] ignore(.idea template), config(aliases, user)

---
+++ git/setup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+cat ~/dotfiles/git/config.ini > ~/.gitconfig
+cat ~/dotfiles/git/ignore/* > ~/.gitignore
+
+NAME=$(getent passwd ${USER} | cut -d':' -f5 | cut -d',' -f1);NAME=${NAME:-$USER};
+
+git config --global user.name "${NAME}"
+git config --global user.email "${USER}@gmail.com"
+printf "[git]   \tUser ${NAME} <${USER}@gmail.com> was configured\n"
