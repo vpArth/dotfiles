abded044834a9bfa760399b7d20125c80b1cb5ab
Alexander Deider <arth.inbox@gmail.com>
2019-03-06 21:56:31 +0700

    [git] Avoid resetting user.name/user.email config

--- git/setup.sh
+++ git/setup.sh
@@ -1,10 +1,21 @@
 #!/usr/bin/env bash
 
+NAME=$(git config --global 'user.name')
+if test -z "${NAME}"; then
+    NAME=$(getent passwd ${USER} | cut -d':' -f5 | cut -d',' -f1)
+    if test -z "${NAME}"; then
+        NAME=${USER}
+    fi
+fi
+
+EMAIL=$(git config --global 'user.email');
+if test -z "${EMAIL}"; then
+ EMAIL="${USER}@gmail.com"
+fi
+
 cat ~/dotfiles/git/config.ini > ~/.gitconfig
 cat ~/dotfiles/git/ignore/* > ~/.gitignore
-
-NAME=$(getent passwd ${USER} | cut -d':' -f5 | cut -d',' -f1);NAME=${NAME:-$USER};
-
 git config --global user.name "${NAME}"
-git config --global user.email "${USER}@gmail.com"
-printf "[git]\tUser ${NAME} <${USER}@gmail.com> was configured\n"
+git config --global user.email "${EMAIL}"
+
+printf "[git]\tUser ${NAME} <${EMAIL}> was configured\n"
