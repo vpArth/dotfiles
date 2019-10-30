0c1939122c7a0285582d9293a8559777d4256d11
Alexander Deider <arth.inbox@gmail.com>
2019-10-30 16:03:34 +0700

    [zsh] Add yarn-autocompletions

--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -4,5 +4,5 @@ export ZSH="$HOME/.oh-my-zsh"
 
 ZSH_THEME=${ZSH_THEME:-custom}
 
-plugins=( git docker yarn tmux exec-time)
+plugins=( git docker yarn tmux exec-time yarn-autocompletions)
 source ${ZSH}/oh-my-zsh.sh
